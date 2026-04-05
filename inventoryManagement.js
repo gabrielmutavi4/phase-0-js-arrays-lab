f6173d4effc0926cef8548919215e78b557302a3
gabrielmutavi4-GM <gabrielmutavi4@gmail.com>
2026-04-05 20:36:44 +0300

    fix product function errors

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,22 +1,33 @@
 // Write your code here
-let products =["Laptop", "Phone", "Headphones", "Monitor"];
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
-function logFirstProduct (products) {
-  console.log(products);
+function logFirstProduct(products) {
+  if (products.length > 0) {
+    console.log(products[0]);
+  } else {
+    console.log("products array is empty.");
+  }
 }
 
-function addproduct (newProduct) {
-  newProduct =(arqument);
+function addProduct(products, newProduct) {
+  products.push(newProduct);
 }
 
-function updateProductName (products, newName) {
-  products.newName = (arguments);
+function updateProductName(products, index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;
+  } else {
+    console.log("Invalid product index.");
+  }
 }
 
-function removeLastProduct (products) {
-  console.log(products)
+function removeLastProduct(products) {
+  if (products.length > 0) {
+    products.pop();
+  } else {
+    console.log("The products array is already empty.");
+  }
 }
-
 
 // Export the necessary parts for testing
 module.exports = {
